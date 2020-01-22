8e914f9e2ce7ede08238d57b37d6febac8b343c9
Madeleine Marcano <madeleine1302@gmail.com>
2020-01-21 23:45:33 -0300

    pruebas de router con métodos

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 //import express from 'express'
-
 const express = require('express') //forma de traer los modulos
+const router = express.Router(); //nos permite separar cabeceras, métodos, peticiones, url.
 
 var app = express(); //inicializar 
 
-app.use('/', function(req, res) {
-    res.send('Hola');
+app.use(router);
 
-})
+router.get("/", function(req, res) {
+    res.send("peticion de get");
+});
+
+router.post("/", function(req, res) {
+    res.send("peticion de post");
+});
+
+router.patch("/", function(req, res) {
+    res.send("peticion de patch");
+});
+
+router.delete("/", function(req, res) {
+    res.send("peticion de delete");
+});
+
+// app.use('/', function(req, res) {
+//    res.send('Hola');
+// })
 
 app.listen(3000);
 
